3c6169e81480e9a1f3750d2cff87b99d0f81bc23
Eric Langendorff <eric.langendorff@asiafoundation.org>
2022-05-29 23:00:51 -0700

    Made welcome page

--- app.py
+++ app.py
@@ -1,12 +1,65 @@
-# Import flask dependency
-from flask import Flask
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# Create new Flask app instance
+# Set Up Database
+filepath = ''
+engine = create_engine(f"sqlite://{filepath}/hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+session = Session(engine)
+
+# Set Up Flask
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
+version_prefix = '/api/v1.0'
 
-def hello_world():
-    return 'Hello world!'
+# 9.5.2 Create the Welcome Route
+@app.route("/")
+
+# def welcome():
+#     return(
+#     '''
+#     Welcome to the Climate Analysis API!
+#     Available Routes:
+#     /api/v1.0/precipitation
+#     /api/v1.0/stations
+#     /api/v1.0/tobs
+#     /api/v1.0/temp/start/end
+#     ''')
+
+# def welcome():
+#     return(
+#         '''
+#         Welcome to the Climate Analysis API!
+#         Available Routes:
+#         /api/v1.0/precipitation
+#         /api/v1.0/stations
+#         /api/v1.0/tobs
+#         /api/v1.0/temp/start/end
+#         '''
+#     )
+
+def welcome():
+    return(
+        f'''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        {version_prefix}/precipitation
+        {version_prefix}/stations
+        {version_prefix}/tobs
+        {version_prefix}/temp/start/end
+        '''
+    )
 
